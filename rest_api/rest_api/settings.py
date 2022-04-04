435f304f6cc45e875396ada9b1922255615905f1
GastonAQS <gastonq98@gmail.com>
2022-04-04 20:12:18 -0300

    collect static

--- rest_api/rest_api/settings.py
+++ rest_api/rest_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +25,9 @@ SECRET_KEY = 'django-insecure-t%d_jtqmoqo!oo+wto6)%wjm8j&*cfl=i02r9wdcf40y=wp)%7
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 ALLOWED_HOSTS = []
 
