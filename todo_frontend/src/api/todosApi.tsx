7ab4f34edc411b064ca0f636a14a578eb123c2ca
GastonAQS <gastonq98@gmail.com>
2022-04-04 16:25:53 -0300

    port edit

--- todo_frontend/src/api/todosApi.tsx
+++ todo_frontend/src/api/todosApi.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const todosApi = axios.create({
-  baseURL: "http://localhost:8001/api/folder/",
+  baseURL: "http://localhost:8000/api/folder/",
   headers: { Accept: "application/json" },
 });
 
